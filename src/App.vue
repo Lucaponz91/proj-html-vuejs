67b798b1127eea4ec338705b3af57571d8525dc8
Lucaponz91 <lucapo1991@msn.com>
2022-09-14 14:20:58 +0200

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComponent :headerLinks="headerLinks" />
+    <MainComponent />
+    <FooterComponent />
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+  },
+  data() {
+    return {
+      headerLinks: [
+        {
+          text: "HOME",
+          href: "noref",
+        },
+        {
+          text: "ABOUT US",
+          href: "noref",
+        },
+        {
+          text: "FACILITIES",
+          href: "noref",
+        },
+        {
+          text: "MEMBERSHIP",
+          href: "noref",
+        },
+        {
+          text: "TESTIMONIALS",
+          href: "noref",
+        },
+        {
+          text: "BLOG",
+          href: "noref",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="header_container">
+    <section
+      class="navbar d-flex flex-row justify-content-between align-items-center"
+    >
+      <div class="logo">
+        <img src="../assets/images/gym_logo_1x.png" alt="gym_logo" />
+      </div>
+      <div class="header_list d-flex flex-row align-items-center">
+        <ul class="d-flex flex-row">
+          <li v-for="link in headerLinks" :key="link.text">
+            <a :href="link.href"> {{ link.text }}</a>
+          </li>
+        </ul>
+      </div>
+      <div class="button">
+        <a href=""><span class="join_today">JOIN TODAY</span></a>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    headerLinks: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header_container {
+  // background-color: #111315;
+  background-image: url("../assets/images/home-banner.jpg");
+  height: 1079px;
+  .navbar{
+    padding-inline: 75px;
+    padding-top: 30px;
+  }
+}
+ul {
+  list-style: none;
+  margin: 0%;
+}
+li {
+    padding-inline: 40px;
+  color: white;
+  a {
+    text-decoration: none;
+    color: white;
+  }
+}
+.button {
+  a {
+    padding: 17px 38px;
+    background-color: #EE6059;
+    text-decoration: none;
+  }
+}
+.join_today {
+  color: white;
+}
+</style>
